32bedbed5dfb4eba0fdb57acf3f407c38766a6be
Benjamin stuart <Benjamin.stuart001@outlook.com>
2022-06-20 15:26:50 +1000

    Initialising portfolio-new

---
+++ src/components/page.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const page = ({bgColor}) => {
+  return (
+    <>
+    <section className="w-screen h-screen">
+        <p>
+          Page
+        </p>
+    </section>
+    </>
+  )
+}
+
+export default page
